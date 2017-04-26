1a4e5010d065fe49ca37ea4f9c56b097a165977a
Mohammed Zajbee <mohazajbee@gmail.comclr>
2017-04-25 21:49:59 -0400

    add method for exercise 4

--- src/method/MethodPracticeForShakilKhanSir.java
+++ src/method/MethodPracticeForShakilKhanSir.java
@@ -5,60 +5,65 @@ import java.util.ArrayList;
 public class MethodPracticeForShakilKhanSir {
 
 	public static void main(String[] args) {
-		//System.out.println(getName("myName"));
-		//System.out.println(getEvenNumber(20));
-		System.out.println(getOddNumber(20));
-		
+		// System.out.println(getName("myName"));
+		// System.out.println(getEvenNumber(20));
+		// System.out.println(getOddNumber(20));
+		System.out.println(getKilometer(12));
 
 	}
+
 	/**
-	 * Question number: 3: Write a method that take a number input and return the list of all odd number from 0 to that given number
-	 * parameter: int number
-	 * returnType: ArrayList<Integer>
-	 * servingBucket: lsit
+	 * Question number: 4: Write a method that take input as mile and return as
+	 * kilometer parameter: double mile returnType: double servingBucket:
+	 * variable
 	 */
-	public static ArrayList<Integer> getOddNumber(int number){
-		
-		
-		
+	public static double getKilometer(double mile) {
+
+		double kilometer = 0;
+
+		kilometer = mile * 1.61;
+
+		return kilometer;
+	}
+
+	/**
+	 * Question number: 3: Write a method that take a number input and return
+	 * the list of all odd number from 0 to that given number parameter: int
+	 * number returnType: ArrayList<Integer> servingBucket: lsit
+	 */
+	public static ArrayList<Integer> getOddNumber(int number) {
+
 		ArrayList<Integer> oddNumber = new ArrayList<Integer>();
-		
-		for ( int i=0; i<number; i=i+1 ){
-			
-			if ( i % 2 == 1 ){
-				
+
+		for (int i = 0; i < number; i = i + 1) {
+
+			if (i % 2 == 1) {
+
 				oddNumber.add(i);
 			}
 		}
-		
-		
-		
+
 		return oddNumber;
 	}
-	
-	
+
 	/**
-	 * Question number:2: Write a method that take a number input and return the list of all even number from 0 to that given number
-	 * parameter: int number
-	 * returnType: ArrayList<Integer>
-	 * servingBucket: list
+	 * Question number:2: Write a method that take a number input and return the
+	 * list of all even number from 0 to that given number parameter: int number
+	 * returnType: ArrayList<Integer> servingBucket: list
 	 */
-	public static ArrayList<Integer> getEvenNumber (int number){
+	public static ArrayList<Integer> getEvenNumber(int number) {
 		ArrayList<Integer> evenNumber = new ArrayList<Integer>();
-		
-		for (int i=0; i<=number; i=i+2){
-			
-			if(i % 2 == 0);
-				evenNumber.add(i);
-			
-			
+
+		for (int i = 0; i <= number; i = i + 2) {
+
+			if (i % 2 == 0)
+				;
+			evenNumber.add(i);
+
 		}
-		
-		
-		
+
 		return evenNumber;
 	}
-	
 
 	/**
 	 * Question number:1: Write a method that take your name input return your
