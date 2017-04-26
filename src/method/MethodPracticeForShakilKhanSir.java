e7e268958ec4aa0431cc629e76fcb37563bffaef
Mohammed Zajbee <mohazajbee@gmail.comclr>
2017-04-25 21:37:51 -0400

    add method for exercise 3

--- src/method/MethodPracticeForShakilKhanSir.java
+++ src/method/MethodPracticeForShakilKhanSir.java
@@ -6,10 +6,37 @@ public class MethodPracticeForShakilKhanSir {
 
 	public static void main(String[] args) {
 		//System.out.println(getName("myName"));
-		System.out.println(getEvenNumber(20));
+		//System.out.println(getEvenNumber(20));
+		System.out.println(getOddNumber(20));
 		
 
 	}
+	/**
+	 * Question number: 3: Write a method that take a number input and return the list of all odd number from 0 to that given number
+	 * parameter: int number
+	 * returnType: ArrayList<Integer>
+	 * servingBucket: lsit
+	 */
+	public static ArrayList<Integer> getOddNumber(int number){
+		
+		
+		
+		ArrayList<Integer> oddNumber = new ArrayList<Integer>();
+		
+		for ( int i=0; i<number; i=i+1 ){
+			
+			if ( i % 2 == 1 ){
+				
+				oddNumber.add(i);
+			}
+		}
+		
+		
+		
+		return oddNumber;
+	}
+	
+	
 	/**
 	 * Question number:2: Write a method that take a number input and return the list of all even number from 0 to that given number
 	 * parameter: int number
